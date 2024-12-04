058a24b5396e80c05456090248f9dbf6018af107
Goutham M <gouthamgo@yahoo.com>
2024-12-04 23:40:01 +1100

    about hashmaps

---
+++ Leetcode75/p1/hashMap.py
@@ -0,0 +1,5 @@
+"""
+A HashMap in Python is equivalent to a dictionary (dict). HashMaps are used to store key-value pairs, where each key is unique, and they are very efficient for operations like lookups, inserts, and deletions, all of which have an average time complexity of O(1).
+
+"""
+
